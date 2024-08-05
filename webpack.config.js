8ac69f4bcf20f8723851d6bba7489932e3fa16b3
Anton <antonprotsyuk@yandex.ru>
2024-08-05 23:35:30 +0300

    fix: rename cistom.html -> index.html

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Budget App',
-            template: 'src/custom.html'
+            template: 'src/index.html'
         }),
         new MiniCssExtractPlugin({
             filename: "bundle.css"
